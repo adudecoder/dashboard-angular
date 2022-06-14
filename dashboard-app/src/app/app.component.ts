0f4b7c9b95ad2d0ff080a6bd015270020ebe9706
Victor Martins <vitorchiesa74@gmail.com>
2022-06-14 16:11:36 -0300

    Message notifications

--- dashboard-app/src/app/app.component.ts
+++ dashboard-app/src/app/app.component.ts
@@ -2,20 +2,21 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { map, Observable, shareReplay } from 'rxjs';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import { PeriodicElement } from './PeriodicElement';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
 })
 export class AppComponent {
+
+  ELEMENT_DATA: PeriodicElement[] = [
+    { },
+  ];
+
+  displayedColumns: string[] = ['position', 'name'];
+  dataSource = this.ELEMENT_DATA;
 
   panelOpenState = false;
 
